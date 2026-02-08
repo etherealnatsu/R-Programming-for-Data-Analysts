f0bc9a16ab3f02e1688ce36ef66c29c9a56be35e
etherealnatsu <142598309+etherealnatsu@users.noreply.github.com>
2026-02-08 14:18:05 -0500

    Update 6. Handling_Missing_Data.R

--- 6. Handling_Missing_Data.R
+++ 6. Handling_Missing_Data.R
@@ -1,5 +1,5 @@
 # Cleaning Messy Data
-
+install.packages("tidyverse")
 
 library(dplyr)
 library(tidyr)
@@ -7,8 +7,16 @@ library(tidyr)
 # Install if you haven't already
 # install.packages("tidyverse")
 
-df <- read.csv("D:\\YouTube\\1. Raw Videos\\R Series\\Files\\Messy_Dataset.csv",
-               na.strings = c("", "NA"))
+#df <- read.csv("D:\\YouTube\\1. Raw Videos\\R Series\\Files\\Messy_Dataset.csv",
+#               na.strings = c("", "NA"))
+df <- read.csv(file.choose(), na.strings = c("", "NA"))
+#file_path <- file.choose()
+#df <- read.csv(file_path, na.strings = c("", "NA"))
+View(df)
+
+
+
+
 
 
 
@@ -19,6 +27,8 @@ df_cleaned <- df %>% drop_na("Email")
 
 #Populate null Numeric Values
 df_cleaned$Transaction_Amount[is.na(df_cleaned$Transaction_Amount)] <- 0
+
+#populate with mean value
 
 
 df_cleaned$Transaction_Amount[is.na(df_cleaned$Transaction_Amount)] <- mean(df_cleaned$Transaction_Amount, na.rm = TRUE)
